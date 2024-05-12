da96c68d06f9ffed0b22ae47768d2b1a395a4bdb
Chris Metivier <chris.metivier@gmail.com>
2024-05-12 18:49:50 -0400

    first full working version

--- main.ts
+++ main.ts
@@ -1,134 +1,176 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile } from "obsidian";
+// import { Plugin, MarkdownView, Notice, App } from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class TagJournalButtonsPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		// create buttons from "togglebutton" codeblocks in preview editor view
+		this.registerMarkdownCodeBlockProcessor(
+			"togglebutton",
+			async (source, el, ctx) => {
+				// create a button html element
+				const button = this.createButton(source, el);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+				// TODO add the "mod-cta" class to any buttons whose
+				// file properties are true at page load
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+				// replace the codeblock with that button element
+				el.replaceWith(button);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// create buttons from "togglebutton" codeblocks in reading view
+		this.registerMarkdownPostProcessor(async (el, ctx) => {
+			// Search for <code> blocks inside this element
+			// in this case, the element defaults to the page container
+			const codeblocks = el.findAll("code");
+
+			// we want to process only the codeblocks that are supposed to
+			// be used by this plugin. we have to loop over all of them
+			for (const codeblock of codeblocks) {
+				// get the text from inside the codeblock
+				const text = codeblock.innerText.trim();
+
+				// for each codeblock, if it has the flag "togglebutton", process it
+				if (text.startsWith("togglebutton")) {
+					// create a button html element
+					const button = this.createButton(text, el);
+
+					// TODO add the "mod-cta" class to any buttons whose
+					// file properties are true at page load
+
+					// replace the codeblock with that button element
+					codeblock.replaceWith(button);
 				}
 			}
 		});
+	}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+	private getArgs(source: string) {
+		// the text (source) inside our codeblock is just a string, but we need
+		// something we can work with, so build an object from it.
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// create an array from the source string by splitting it at line breaks
+		const split = source.trim().split("\n");
+
+		// init an empty object to fill with our source's key-value pairs
+		const args: { [key: string]: string } = {};
+
+		// build an object of key value pairs from each element of the split array
+		for (const line of split) {
+			// split each line at the colon
+			const keyValuePair: string[] = line.split(":");
+
+			// the first element will be the key of the property
+			const key: string = keyValuePair[0].toLowerCase().trim();
+
+			// the second element will be the value of the property
+			const value: string = keyValuePair[1].trim();
+
+			// add that key and value to our object
+			args[key] = value;
+		}
+
+		// return the completed object
+		return args;
+	}
+
+	/**
+	 * a function to get the current value of a boolean file property
+	 *
+	 * @return  {Promise<boolean>}  the value of the property
+	 */
+	private async getFileProperty(
+		file: TFile,
+		propertyName: string
+	): Promise<boolean> {
+		return new Promise<boolean>((resolve, reject) => {
+			this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+				const propertyValue = frontmatter[propertyName];
+				if (typeof propertyValue === "boolean") {
+					resolve(propertyValue);
+				} else {
+					resolve(false);
+				}
+			});
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
-
+	/**
+	 * a function to toggle the value of a boolean file property and
+	 * return its new value
+	 *
+	 * @return  {Promise<boolean>}  the new value of the property
+	 */
+	private async toggleFileProperty(
+		file: TFile,
+		propertyName: string
+	): Promise<boolean> {
+		return new Promise<boolean>((resolve, reject) => {
+			this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+				const propertyValue = frontmatter[propertyName];
+				if (typeof propertyValue === "boolean") {
+					frontmatter[propertyName] = !propertyValue;
+					resolve(!propertyValue);
+				} else {
+					resolve(false);
+				}
+			});
+		});
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+	private createButton(args: string, el: HTMLElement): HTMLElement {
+		// get the current file. we need it in order to interact with file properties
+		// and if we can't get it, abort the whole function;
+		const file = this.app.workspace.getActiveFile();
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+		if (file) {
+			// parse the arguments
+			const argsObject = this.getArgs(args);
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+			// assign the property name to a var for convenience
+			const propertyName = argsObject.property;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+			//create the button element
+			const button = el.createEl("button", {
+				text: argsObject.name,
+				cls: ["button-inline"],
+			});
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+			// if the button codeblock includes an id value, add it as the button's id
+			if (argsObject.id) {
+				button.setAttribute("id", argsObject.id);
+			}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+			// if the button configincludes the name of a property to toggle
+			// add a click handler to the button
+			if (propertyName) {
+				button.on("click", "button", async () => {
+					// toggleFileProperty toggles the given property and returns its new value
+					// or returns false if the property doesn't exist
+					const newPropertyValue = await this.toggleFileProperty(
+						file,
+						propertyName
+					);
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+					// console.log("newPropertyValue", newPropertyValue);
 
-	display(): void {
-		const {containerEl} = this;
+					if (newPropertyValue) {
+						// if the new property value is true, add the native button class "mod-cta"
+						// to change its appearance to look active
+						button.classList.add("mod-cta");
+					} else {
+						// if the new property value is false, remove that class
+						button.classList.remove("mod-cta");
+					}
+				});
+			}
 
-		containerEl.empty();
+			return button;
+		}
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// if the current file is null, output an error message.
+		return el.createEl("span", {
+			text: "togglebutton error: active file is null",
+		});
 	}
 }
